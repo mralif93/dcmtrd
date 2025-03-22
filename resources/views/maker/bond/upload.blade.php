9c848986234b971b0849301f318228f85719d719
Muhammad Alif Bin Tajudin <m.alif.tajudin@muamalat.com.my>
2025-03-22 10:42:40 +0800

    Updated user flow bond module

--- resources/views/maker/bond/upload.blade.php
+++ resources/views/maker/bond/upload.blade.php
@@ -74,7 +74,7 @@
 
                         <!-- Form Actions -->
                         <div class="flex items-center justify-end">
-                            <a href="{{ route('bonds-info.index') }}" 
+                            <a href="{{ route('bond-m.details', $issuer) }}" 
                                class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                 Cancel
                             </a>
