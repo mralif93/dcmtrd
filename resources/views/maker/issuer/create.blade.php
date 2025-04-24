f42395543fa9f2b7ef9917610021085d842bc146
Muqri Amin Bin Mohd Shamsuddin <muqri.amin@artrustees.com.my>
2025-04-24 16:09:25 +0800

    Update: change cancel button redirect to issuers idex page

--- resources/views/maker/issuer/create.blade.php
+++ resources/views/maker/issuer/create.blade.php
@@ -180,7 +180,7 @@
 
                     <!-- Form Actions -->
                     <div class="flex justify-end gap-4 pt-6 border-t border-gray-200">
-                        <a href="{{ route('dashboard') }}"
+                        <a href="{{ route('maker.dashboard', ['section' => 'dcmtrd']) }}"
                             class="inline-flex items-center px-4 py-2 font-medium text-gray-700 bg-gray-200 border border-transparent rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                             Cancel
                         </a>
