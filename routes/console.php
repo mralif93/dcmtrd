0b469a503595da3c368182a482c45d2575484d78
Muqri Amin Bin Mohd Shamsuddin <muqri.amin@artrustees.com.my>
2025-04-28 17:53:40 +0800

    Update: run cron job

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\SendPaymentScheduleReminders;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('app:send-payment-schedule-reminders', function () {
+    $this->call(SendPaymentScheduleReminders::class);
+})->purpose('Send payment schedule reminders')->daily()->withoutOverlapping();
