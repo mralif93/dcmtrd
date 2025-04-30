ca9f094252d9bd04191180e683cc46015aa5d1ce
Muqri Amin Bin Mohd Shamsuddin <muqri.amin@artrustees.com.my>
2025-04-30 12:10:13 +0800

    Update: run cron job use console

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use App\Console\Commands\SendPaymentScheduleReminders;
+use App\Console\Commands\SendTrusteeFeeReminderEmail;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
@@ -11,3 +12,7 @@ use App\Console\Commands\SendPaymentScheduleReminders;
 Artisan::command('app:send-payment-schedule-reminders', function () {
     $this->call(SendPaymentScheduleReminders::class);
 })->purpose('Send payment schedule reminders')->daily()->withoutOverlapping();
+
+Artisan::command('app:send-trustee-fee-reminder-email', function () {
+    $this->call(SendTrusteeFeeReminderEmail::class);
+})->purpose('Send trustee fee reminder emails')->daily()->withoutOverlapping();
