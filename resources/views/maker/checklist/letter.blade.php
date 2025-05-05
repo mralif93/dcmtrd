ce157e7a4b3373297b06a1c3edd68cd510adfee0
Muhammad Alif Bin Tajudin <m.alif.tajudin@muamalat.com.my>
2025-05-05 08:55:59 +0800

    Updated reits module

--- resources/views/maker/checklist/letter.blade.php
+++ resources/views/maker/checklist/letter.blade.php
@@ -3,8 +3,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6">
-                    <!-- Print Button -->
-                    <div class="print-controls mb-4 flex justify-end">
+                    <!-- Print and Back Buttons -->
+                    <div class="print-controls mb-4 flex justify-between">
+                        <a href="{{ route('checklist-m.index', $checklist->siteVisit->property) }}" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                            <i class="fa fa-arrow-left mr-2"></i> Back
+                        </a>
                         <button id="printButton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             <i class="fa fa-print mr-2"></i> Print Checklist
                         </button>
