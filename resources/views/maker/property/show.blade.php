14f26893403c8f13d95c441d3a657433a931d1af
Muhammad Alif Bin Tajudin <m.alif.tajudin@muamalat.com.my>
2025-05-09 00:22:45 +0800

    Updated reits module

--- resources/views/maker/property/show.blade.php
+++ resources/views/maker/property/show.blade.php
@@ -165,7 +165,7 @@
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"></path>
                                         </svg>
                                         <a href="{{ asset('storage/' . $property->master_lease_agreement) }}" target="_blank" class="text-indigo-600 hover:text-indigo-900">
-                                            {{ basename($property->master_lease_agreement) }}
+                                            Download
                                         </a>
                                     </div>
                                 @else
@@ -182,7 +182,7 @@
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"></path>
                                         </svg>
                                         <a href="{{ asset('storage/' . $property->valuation_report) }}" target="_blank" class="text-indigo-600 hover:text-indigo-900">
-                                            {{ basename($property->valuation_report) }}
+                                            Download
                                         </a>
                                     </div>
                                 @else
