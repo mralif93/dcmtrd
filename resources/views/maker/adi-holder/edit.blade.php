6e0a78db5950d5c1e4ecf3e3391ec5c0dae98e6b
Muqri Amin Bin Mohd Shamsuddin <muqri.amin@artrustees.com.my>
2025-05-09 10:34:43 +0800

    Update: make sure back to previous pages

--- resources/views/maker/adi-holder/edit.blade.php
+++ resources/views/maker/adi-holder/edit.blade.php
@@ -81,7 +81,7 @@
                     </div>
 
                     <div class="flex justify-end gap-4 pt-6">
-                        <a href="{{ route('bond-m.details', $facilities) }}"
+                        <a href="{{ route('facility-info-m.show', $facilities) }}"
                             class="inline-flex items-center px-4 py-2 font-medium text-gray-700 bg-gray-200 border border-transparent rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                             Cancel
                         </a>
