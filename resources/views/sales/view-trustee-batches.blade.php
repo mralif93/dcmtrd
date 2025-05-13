8dfa429d71f448cd42c50f83250c42479b909b36
Muqri Amin Bin Mohd Shamsuddin <muqri.amin@artrustees.com.my>
2025-05-14 02:03:39 +0800

    Update: typo

--- resources/views/sales/view-trustee-batches.blade.php
+++ resources/views/sales/view-trustee-batches.blade.php
@@ -70,7 +70,7 @@
                                     <td class="px-6 py-4">
                                         <div class="flex space-x-3">
                                             {{-- Download Button --}}
-                                            <a href="{{ route('compliance.trustee-reports.download', $batch->id) }}"
+                                            <a href="{{ route('sales.trustee-reports.download', $batch->id) }}"
                                                 class="inline-flex items-center px-3 py-1.5 text-sm font-medium text-green-600 hover:text-green-800">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1"
                                                     fill="none" viewBox="0 0 24 24" stroke="currentColor">
